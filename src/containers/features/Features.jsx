827f5220c4db21d5b7241f2f306c47bba23c65d4
realestever <119549369+realestever@users.noreply.github.com>
2025-08-18 14:35:02 -0700

    Implemented features section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import './features.css';
+import { Feature } from '../../components';
 
-const Features = ({title, text}) => {
+const featuresData = [
+  {
+    title: "Improving end distrusts instantly",
+    text: "From they fineest fishers of the world, we bring you the best quality fish. Our fish are sourced from sustainable fisheries and are delivered fresh to your doorstep.",
+
+  },
+  {
+    title: "Becoming a member of the team",
+    text: "From they fineest fishers of the world, we bring you the best quality fish. Our fish are sourced from sustainable fisheries and are delivered fresh to your doorstep.",
+
+  },
+  {
+    title: "Message or am nothing",
+    text: "From they fineest fishers of the world, we bring you the best quality fish. Our fish are sourced from sustainable fisheries and are delivered fresh to your doorstep.",
+
+  },
+  {
+    title: "Really boy law coiuntrty",
+    text: "From they fineest fishers of the world, we bring you the best quality fish. Our fish are sourced from sustainable fisheries and are delivered fresh to your doorstep.",
+
+  }
+]
+
+const Features = () => {
   return (
-    <div>
-      
-    </div>
+      <div className='gpt3__features section__padding' id='features'>
+        <div className='gpt3__features-heading'>
+          <h1 className='gradient__text'>The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+          <p>Request Early Access to Get Started</p>
+        </div>
+        <div className='gpt3__features-container'>
+          {featuresData.map((item, index)=>(
+            <Feature title={item.title} text={item.text} key={item.title + index}/>
+          )
+          )}
+        </div>
+      </div>
   )
 }
 
